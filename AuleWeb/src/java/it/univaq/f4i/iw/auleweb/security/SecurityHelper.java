bb624944ad80ba25a845f62f14dec726ed0296f1
FrancoF22 <francesco.falleroni@student.univaq.it>
2023-09-22 15:50:50 +0200

    scrittura per il connection pooling

--- AuleWeb/src/java/it/univaq/f4i/iw/auleweb/security/SecurityHelper.java
+++ AuleWeb/src/java/it/univaq/f4i/iw/auleweb/security/SecurityHelper.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-package it.univaq.f4i.iw.auleweb.controller.it.univaq.f4i.iw.auleweb.security;
+package it.univaq.f4i.iw.auleweb.security;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
