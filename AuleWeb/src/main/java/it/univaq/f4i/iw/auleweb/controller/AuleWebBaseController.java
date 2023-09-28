9e330d57996177db6af9b201a6240012c567dcff
FrancoF22 <francesco.falleroni@student.univaq.it>
2023-09-28 10:44:55 +0200

    creazione progetto (con maven)

---
+++ AuleWeb/src/main/java/it/univaq/f4i/iw/auleweb/controller/AuleWebBaseController.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package it.univaq.f4i.iw.auleweb.controller;
+
+import it.univaq.f4i.iw.framework.controller.AbstractBaseController;
+import it.univaq.f4i.iw.framework.data.DataLayer;
+import java.sql.SQLException;
+import javax.servlet.ServletException;
+import javax.sql.DataSource;
+
+/**
+ *
+ * @author franc
+ */
+public abstract class AuleWebBaseController extends AbstractBaseController{
+    /*
+    @Override
+    protected DataLayer createDataLayer(DataSource ds) throws ServletException {
+        try {
+            return new AuleWebDataLayer(ds);
+        } catch (SQLException ex) {
+            throw new ServletException(ex);
+        }
+    } 
+*/
+}
---
+++ AuleWeb/src/main/java/it/univaq/f4i/iw/framework/data/DAO.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package it.univaq.f4i.iw.framework.data;
+
+import java.sql.Connection;
+
+/**
+ *
+ * @author franc
+ */
+public class DAO {
+
+protected final DataLayer dataLayer;
+    protected final Connection connection;
+
+    public DAO(DataLayer d) {
+        this.dataLayer = d;
+        this.connection = d.getConnection();
+    }
+
+    protected DataLayer getDataLayer() {
+        return dataLayer;
+    }
+
+    protected Connection getConnection() {
+        return connection;
+    }
+
+    public void init() throws DataException {
+
+    }
+
+    public void destroy() throws DataException {
+        
+    }
+}
