b53ad8e9d5cca6bbf4fdd7f2030bf3dc3e7ecf1a
FrancoF22 <francesco.falleroni@student.univaq.it>
2023-10-30 19:59:41 +0100

    altre modifche

--- AuleWeb/src/main/java/it/univaq/f4i/iw/auleweb/data/proxy/EventoProxy.java
+++ AuleWeb/src/main/java/it/univaq/f4i/iw/auleweb/data/proxy/EventoProxy.java
@@ -64,7 +64,7 @@ public class EventoProxy extends EventoImpl implements DataItemProxy{
     }
 
     @Override
-    public void setTipo(String t) {
+    public void setTipo(Tipologia t) {
         super.setTipo(t);
         this.modified = true;
     }
