93602f847962be15560e75834442ee3baa1de852
FrancoF22 <francesco.falleroni@student.univaq.it>
2023-11-08 16:51:37 +0100

    scrittura login

--- AuleWeb_Back-UP/src/main/java/it/univaq/f4i/iw/auleweb/controller/AuleWebBaseController.java
+++ AuleWeb_Back-UP/src/main/java/it/univaq/f4i/iw/auleweb/controller/AuleWebBaseController.java
@@ -1,13 +1,25 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
 package it.univaq.f4i.iw.auleweb.controller;
+
+import it.univaq.f4i.iw.auleweb.data.dao.AuleWebDataLayer;
+import it.univaq.f4i.iw.framework.controller.AbstractBaseController;
+import it.univaq.f4i.iw.framework.data.DataLayer;
+import java.sql.SQLException;
+import javax.servlet.ServletException;
+import javax.sql.DataSource;
 
 /**
  *
- * @author franc
+ * @author pcela
  */
-public interface AuleWebBaseController {
-    
+public abstract class AuleWebBaseController extends AbstractBaseController {
+
+    @Override
+    protected DataLayer createDataLayer(DataSource ds) throws ServletException {
+        try {
+            return new AuleWebDataLayer(ds);
+        } catch (SQLException ex) {
+            throw new ServletException(ex);
+        }
+    }
+
 }
--- AuleWeb_Back-UP/src/main/java/it/univaq/f4i/iw/auleweb/controller/Login.java
+++ AuleWeb_Back-UP/src/main/java/it/univaq/f4i/iw/auleweb/controller/Login.java
@@ -4,82 +4,79 @@
  */
 package it.univaq.f4i.iw.auleweb.controller;
 
+import it.univaq.f4i.iw.auleweb.data.dao.AuleWebDataLayer;
+import it.univaq.f4i.iw.auleweb.data.model.Utente;
+import it.univaq.f4i.iw.framework.data.DataException;
+import it.univaq.f4i.iw.framework.result.TemplateManagerException;
+import it.univaq.f4i.iw.framework.result.TemplateResult;
+import it.univaq.f4i.iw.framework.security.SecurityHelpers;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- *
- * @author franc
- */
-public class Login extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+public class Login extends AuleWebBaseController {
+
+    private void action_default(HttpServletRequest request, HttpServletResponse response) throws IOException, TemplateManagerException {
+        TemplateResult result = new TemplateResult(getServletContext());
+        request.setAttribute("referrer", request.getParameter("referrer"));
+        result.activate("login.ftl.html", request, response);
+
+//        //esempio di creazione utente
+//        //create user example
+//        try {
+//            User u = ((NewspaperDataLayer) request.getAttribute("datalayer")).getUserDAO().createUser();
+//            u.setUsername("a");
+//            u.setPassword(SecurityHelpers.getPasswordHashPBKDF2("p"));
+//            ((NewspaperDataLayer) request.getAttribute("datalayer")).getUserDAO().storeUser(u);
+//        } catch (DataException | NoSuchAlgorithmException | InvalidKeySpecException ex) {
+//            Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+    }
+
+    //nota: usente di default nel database: nome a, password p
+    //note: default user in the database: name: a, password p
+    private void action_login(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String username = request.getParameter("u");
+        String password = request.getParameter("p");
+
+        if (!username.isEmpty() && !password.isEmpty()) {
+            try {
+                Utente u = ((AuleWebDataLayer) request.getAttribute("datalayer")).getUtenteDAO().getUtenteByEmail(username);
+                if (u != null && SecurityHelpers.checkPasswordHashPBKDF2(password, u.getPassword())) {
+                    SecurityHelpers.createSession(request, username, u.getKey());
+                    if (request.getParameter("referrer") != null) {
+                        response.sendRedirect(request.getParameter("referrer"));
+                    } else {
+                        response.sendRedirect("home");
+                    }
+                    return;
+                }
+            } catch (NoSuchAlgorithmException | InvalidKeySpecException | DataException ex) {
+                Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        handleError("Login failed", request, response);
+    }
+    
+    @Override
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try ( PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet Login</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet Login at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+            throws ServletException {
+        try {
+            if (request.getParameter("login") != null) {
+                action_login(request, response);
+            } else {
+                String https_redirect_url = SecurityHelpers.checkHttps(request);
+                request.setAttribute("https-redirect", https_redirect_url);
+                action_default(request, response);
+            }
+        } catch (IOException | TemplateManagerException ex) {
+            handleError(ex, request, response);
         }
     }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
-
 }
