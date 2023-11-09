ffa7082a1fa379049e7185bb05b3409eeee1da8a
Paolo Celani <pcelani03@hotmail.com>
2023-11-09 17:16:11 +0100

    pagina home

--- AuleWeb_Back-UP/src/main/java/it/univaq/f4i/iw/auleweb/controller/Home.java
+++ AuleWeb_Back-UP/src/main/java/it/univaq/f4i/iw/auleweb/controller/Home.java
@@ -1,36 +1,29 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
- */
 package it.univaq.f4i.iw.auleweb.controller;
 
 import it.univaq.f4i.iw.auleweb.data.dao.AuleWebDataLayer;
 import it.univaq.f4i.iw.framework.data.DataException;
 import it.univaq.f4i.iw.framework.result.TemplateManagerException;
 import it.univaq.f4i.iw.framework.result.TemplateResult;
-import java.io.*;
+import it.univaq.f4i.iw.framework.security.SecurityHelpers;
+import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- *
- * @author franc
- */
+//chiamare home.java
 public class Home extends AuleWebBaseController {
 
     private void action_default(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException, TemplateManagerException {
         try {
             TemplateResult res = new TemplateResult(getServletContext());
-            request.setAttribute("page_title", "Home");
+            request.setAttribute("page_title", "Dipartimenti");
             request.setAttribute("ListaDipartimenti", ((AuleWebDataLayer) request.getAttribute("datalayer")).getGruppoDAO().getAllGruppi());
             res.activate("home.ftl.html", request, response);
         } catch (DataException ex) {
             handleError("Data access exception: " + ex.getMessage(), request, response);
         }
     }
-    
-    /*
+
     private void action_choose(HttpServletRequest request, HttpServletResponse response, int gruppo_key) throws IOException, ServletException, TemplateManagerException {
         try {
             TemplateResult res = new TemplateResult(getServletContext());
@@ -41,7 +34,7 @@ public class Home extends AuleWebBaseController {
             handleError("Data access exception: " + ex.getMessage(), request, response);
         }
     }
-    
+
     @Override
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException {
@@ -62,28 +55,10 @@ public class Home extends AuleWebBaseController {
         }
 
     }
-    
-    */
-    @Override
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException {
 
-        try {
-            action_default(request, response);
-
-        } catch (IOException | TemplateManagerException ex) {
-            handleError(ex, request, response);
-        }
-    }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
     @Override
     public String getServletInfo() {
         return "Main AuleWeb servlet";
-    }// </editor-fold>
+    }
 
 }
