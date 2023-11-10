869d58683232bf54f51024cf8578ac67153d8076
FrancoF22 <francesco.falleroni@student.univaq.it>
2023-11-10 16:40:14 +0100

    aggiunta roba per logica ripetizione eventi (calendario)

---
+++ AuleWeb_Back-UP/src/main/java/it/univaq/f4i/iw/auleweb/data/dao/CalendarioDAO.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package it.univaq.f4i.iw.auleweb.data.dao;
+
+/**
+ *
+ * @author franc
+ */
+public interface CalendarioDAO {
+    
+}
